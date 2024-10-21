76f99b25fb4a8b7d266817fcc2688d09ce7a72c0
Morar-David <daviddavid288@yahoo.com>
2024-10-21 11:02:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 # This is a sample Python script.
 
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 
 print('David Morar')
-print('am fost afara')
+
+
+t1="Liga Națiunilor. Cipru-Romania 0-3: Tricolorii au trei victorii "
+t2="din trei meciuri cu Lucescu antrenor și conduc grupa"
+
+def articol(text1):
+    print(text1.upper())
+    print(text1.strip())
+    print(text1.swapcase())
+    print(text1.title())
+    print(t1+t2)
+
+articol(t1)
+articol(t2)
